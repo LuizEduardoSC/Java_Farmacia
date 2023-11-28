91a103ca620d4bf34e0c1360ccac338fa6acda11
LuizEduardoSC <luizeduardoedd1@gmail.com>
2023-11-28 13:02:50 -0300

    Criar Model e repository

---
+++ src/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package farmacia.repository;
+
+import farmacia.model.Produto;
+
+public interface ProdutoRepository {
+	
+	public void procurarPorNumero(int numero);
+	public void listarTodas();
+	public void cadastrar(Produto produto);
+	public void atualizar(Produto produto);
+	public void deletar(int numero);
+
+}
