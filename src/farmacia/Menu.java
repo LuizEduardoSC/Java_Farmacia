cdd2c3f11cbf3682460a8ca705e1dec49df90fbc
LuizEduardoSC <luizeduardoedd1@gmail.com>
2023-12-07 01:02:58 -0300

    Projeto Farmacia Corrigido

--- src/farmacia/Menu.java
+++ src/farmacia/Menu.java
@@ -1,76 +1,166 @@
 package farmacia;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Optional;
 import java.util.Scanner;
 
+import farmacia.controller.ProdutoController;
+import farmacia.model.Medicamento;
+import farmacia.model.Produto;
 
-		public class Menu {
+public class Menu {
 
-			public static void main(String[] args) {
-				
-				Scanner leia = new Scanner(System.in);
-				
-				int opcao;
-				
-				while (true) {
-					
-				System.out.println();
-				
-				System.out.println("*************************************************");		
-				System.out.println("                                                 ");		
-				System.out.println("              FARMÁCIA - BOM PREÇO               ");		
-				System.out.println("                                                 ");		
-				System.out.println("*************************************************");		
-				System.out.println("                                                 ");
-				System.out.println("          1 - Criar Produto                      ");
-				System.out.println("          2 - Listar todos os Produtos           ");
-				System.out.println("          3 - Buscar Produto por Número          ");
-				System.out.println("          4 - Atualizar Dados de um Produto      ");
-				System.out.println("          5 - Apagar Produto                     ");
-				System.out.println("          6 - Sair                               ");
-				System.out.println("                                                 ");		
-				System.out.println("*************************************************");
-				System.out.println("Entre com a opção desejada                       ");
-				System.out.println("                                                 ");
-				
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		
+		ProdutoController produtos = new ProdutoController();
+		
+		int opcao, id, tipo;
+		String nome, generico;
+		float preco;
+
+		while (true) {
+
+			System.out.println("*************************************************");
+			System.out.println("                                                 ");
+			System.out.println("              FARMÁCIA - BOM PREÇO               ");
+			System.out.println("                                                 ");
+			System.out.println("*************************************************");
+			System.out.println("                                                 ");
+			System.out.println("          1 - Criar Produto                      ");
+			System.out.println("          2 - Listar todos os Produtos           ");
+			System.out.println("          3 - Buscar Produto por Número          ");
+			System.out.println("          4 - Atualizar Dados de um Produto      ");
+			System.out.println("          5 - Apagar Produto                     ");
+			System.out.println("          6 - Sair                               ");
+			System.out.println("                                                 ");
+			System.out.println("*************************************************");
+			System.out.println("Entre com a opção desejada                       ");
+			System.out.println("                                                 ");
+
+			try {
 				opcao = leia.nextInt();
-				
-				if(opcao == 6) {
-					System.out.println( "\nFarmácia Bom Preço - O Melhor preço é aqui!");
-					sobre();
-					leia.close();
-					System.exit(0);
-				}
-				
-				switch (opcao) {
-				case 1: System.out.println( "Criar Produto\n\n");
-				break;
-				
-				case 2: System.out.println( "Listar todas os Produtos\n\n");
-				break;
-				
-				case 3: System.out.println( "Consultar dados de um Produto - por id \n\n");
-				break;
-				
-				case 4: System.out.println( "Atualizar dados de um Produto\n\n");
-				break;
-				
-				case 5: System.out.println( "Apagar Produtoa\n\n");
-				break;
-				
-				
-				default:
-					System.out.println(  "\nOpção Inválida! \n" );
-					break;
-				}
-
-			}
-			}
-			public static void sobre() {
-				System.out.println("\n*************************************************");
-				System.out.println("Projeto Desenvolvido por: Luiz Eduardo!");
-				System.out.println("Luiz Eduardo - luizeduardoedd1@gmail.com");
-				System.out.println("github.com/LuizEduardoSC");
-				System.out.println("*************************************************");
+			} catch (InputMismatchException e) {
+				System.out.println("Digite valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
 			}
 
+			if (opcao == 6) {
+				System.out.println("\nFarmácia Bem Estar - Remédio Barato é aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println("Criar Produto\n\n");
+
+				System.out.println("Digite o nome do Produto: ");
+				leia.skip("\\R");
+				nome = leia.nextLine();
+
+				// System.out.println("Digite o tipo do Produto (1 - Medicamentos): ");
+				tipo = 1;
+
+				System.out.println("Digite o Preço do Produto: ");
+				preco = leia.nextFloat();
+
+				System.out.println("Digite o nome Genérico: ");
+				leia.skip("\\R");
+				generico = leia.nextLine();
+
+				produtos.cadastrar(new Medicamento(produtos.gerarNumero(), nome, tipo, preco, generico));
+
+				keyPress();
+				break;
+			case 2:
+				System.out.println("Listar todos os Produtos\n\n");
+
+				produtos.listarTodas();
+
+				keyPress();
+				break;
+			case 3:
+				System.out.println("Consultar dados de um Produto - por id\n\n");
+
+				System.out.println("Digite o Id do Produto: ");
+				id = leia.nextInt();
+
+				produtos.procurarPorNumero(id);
+
+				keyPress();
+				break;
+			case 4:
+				System.out.println("Atualizar dados de um Produto\n\n");
+
+				System.out.println("Digite o Id do Produto: ");
+				id = leia.nextInt();
+
+				Optional<Produto> produto = produtos.buscarNaCollection(id);
+
+				if (produto.isPresent()) {
+
+					System.out.println("Digite o nome do Produto: ");
+					leia.skip("\\R");
+					nome = leia.nextLine();
+
+					tipo = produto.get().getTipo();
+
+					System.out.println("Digite o Preço do Produto: ");
+					preco = leia.nextFloat();
+
+					System.out.println("Digite o nome Genérico: ");
+					leia.skip("\\R");
+					generico = leia.nextLine();
+
+					produtos.atualizar(new Medicamento(id, nome, tipo, preco, generico));
+
+				} else
+					System.out.println("O Produto não foi encontrado!");
+
+				keyPress();
+				break;
+			case 5:
+				System.out.println("Apagar Produto\n\n");
+
+				System.out.println("Digite o Id do Produto: ");
+				id = leia.nextInt();
+
+				produtos.deletar(id);
+
+				keyPress();
+				break;
+			default:
+				System.out.println("\nOpção Inválida!\n");
+				break;
+			}
+		}
 	}
+
+	public static void sobre() {
+		System.out.println("\n*************************************************");
+		System.out.println("Projeto Desenvolvido por: Luiz Eduardo!");
+		System.out.println("Luiz Eduardo - luizeduardoedd1@gmail.com");
+		System.out.println("github.com/LuizEduardoSC");
+		System.out.println("*************************************************");
+	}
+	
+	public static void keyPress() {
+		
+		try {
+			
+			System.out.println("\n\nPressione a tecla Enter para continuar...");
+			System.in.read();
+			
+		}catch(IOException e){
+			
+			System.out.println("Você pressionou uma tecla inválida!");
+			
+		}
+	}
+}
+
--- src/farmacia/repository/ProdutoRepository.java
+++ src/farmacia/repository/ProdutoRepository.java
@@ -3,11 +3,15 @@ package farmacia.repository;
 import farmacia.model.Produto;
 
 public interface ProdutoRepository {
-	
+
 	public void procurarPorNumero(int numero);
+
 	public void listarTodas();
+
 	public void cadastrar(Produto produto);
+
 	public void atualizar(Produto produto);
+
 	public void deletar(int numero);
 
 }
